e2132702c278a874b70dfb76f664530231bbdced
AlesiaMianitskaya <alesya.menitskaya@mail.ru>
2019-03-23 16:40:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+  const h = 50,
+    q = 25,
+    d = 10,
+    n = 5,
+    p = 1;
+
+  let H, Q, D, N, P, coins;
+
+  if (currency <= 0) {
+    return {};
+  }
+
+  if (currency > 10000) {
+    return {
+      error: "You are rich, my friend! We don't have so much coins for exchange"
+    };
+  }
+
+  if (currency / h > 0) {
+    H = Math.floor(currency / h);
+    currency = currency - H * h;
+    if (currency === 0) {
+      coins = { H: H };
+    }
+  }
+
+  if (currency / q > 0) {
+    Q = Math.floor(currency / q);
+    currency = currency - Q * q;
+    if (currency === 0) {
+      coins = { H: H, Q: Q };
+    }
+  }
+
+  if (currency / d > 0) {
+    D = Math.floor(currency / d);
+    currency = currency - D * d;
+    if (currency === 0) {
+      coins = { H: H, Q: Q, D: D };
+    }
+  }
+
+  if (currency / n > 0) {
+    N = Math.floor(currency / n);
+    currency = currency - N * n;
+    if (currency === 0) {
+      coins = { H: H, Q: Q, D: D, N: N };
+    }
+  }
+
+  if (currency / p > 0) {
+    P = Math.floor(currency / p);
+    currency = currency - P * p;
+    coins = { H: H, Q: Q, D: D, N: N, P: P };
+  }
+
+  for (var key in coins) {
+    if (coins[key] === 0) {
+      delete coins[key];
+    }
+  }
+  return coins;
+};
